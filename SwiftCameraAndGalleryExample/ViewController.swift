096652036a8f42aa3b52d0a6eaf67bfced5fce1c
MacStudent <macstudent@126-imac19.local>
2019-03-13 19:56:50 -0400

    camera gallery

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnCamera(_ sender: UIBarButtonItem) {
+        
+   
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate;
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func btnGallery(_ sender: UIBarButtonItem) {
+     if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+                let myPickerController = UIImagePickerController()
+                myPickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate;
+                myPickerController.sourceType = .photoLibrary
+                self.present(myPickerController, animated: true, completion: nil)
+            }
+        }
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.dismiss(animated: true, completion: nil)
+        }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imageView.image=image
+            
+        }else{
+            print("Something went wrong")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 }
+
+        
+
+
 
